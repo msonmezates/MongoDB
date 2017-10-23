48a584971d71d07e91a088c391fb5307320f14a3
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-22 20:51:13 -0500

    - Ensure mongoose is connected - add test to create user and validate if it's stored in the database

--- src/user.js
+++ src/user.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
   name: String,
-  required: true
 });
 
 const User = mongoose.model('user', UserSchema);
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,7 +1,17 @@
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost/users_test');
+before((done) => { //before is executed only once to make sure mongoose is connected
+  mongoose.connect('mongodb://localhost/users_test', { useMongoClient: true });
+  mongoose.connection
+    .once('open', () => { done(); })
+    .on('error', (error) => console.warn('Error', error));
+});
 
-mongoose.connection
-  .once('open', () => console.log('Mongoose is connected'))
-  .on('error', (error) => console.warn('Error', error));
+//beforeEach() runs every time we need to start a test
+beforeEach((done) => {
+  mongoose.connection.collections.users.drop(() => { //clears the database before running each test
+    //Ready to run the next test!
+    done();
+  });
+});
