12df8cfcc581568c7a2ff55f47ce7f2ec8d3af59
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-24 15:50:36 -0500

    Add another test to find a user with a particular id

--- test/reading_test.js
+++ test/reading_test.js
@@ -18,4 +18,13 @@ describe('Reading users out of the database', () => {
       })
       .catch(done());
   });
+
+  it('find a user with a particular id', done => {
+    User.findOne({ _id: joe._id })
+      .then(user => {
+        assert(user.name === 'Joe');
+        done();
+      })
+      .catch(done());
+  });
 });
