76226f6b03fb87e817e98c0115a14b1f21fef6e8
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-10-24 19:09:56 -0500

    Add findOneAndRemove function for class method

--- test/delete_test.js
+++ test/delete_test.js
@@ -28,8 +28,13 @@ describe('Deleting a user', () => {
       });
   });
 
-  it('class method findAndRemove', () => {
-
+  it('class method findOneAndRemove', (done) => {
+    User.findOneAndRemove({ name: 'Joe' })
+     .then(() => User.findOne({ name: 'Joe' }))
+     .then(user => {
+       assert(user === null);
+       done();
+     });
   });
 
   it('class method findByIdAndRemove', () => {
