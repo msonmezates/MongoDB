d9209c979e30090d5e791ad3f9339a3a823a2f45
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-03 18:37:50 -0500

    - Require the name and return an error message - Make sure test displays error message correctly

--- test/validation_test.js
+++ test/validation_test.js
@@ -3,6 +3,9 @@ const User = require('../src/user');
 
 describe('Validating records', () => {
   it('requires a user name', () => {
-    
+    const user = new User({ name: undefined }); //name:undefined is written to make it fail
+    const validateResult = user.validateSync(); //we want it synchronized on purpose
+    const { message } = validateResult.errors.name;
+    assert(message === 'Name is required.');
   });
 });
