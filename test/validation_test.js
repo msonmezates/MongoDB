19f5bf957e67fa942210d02a004810abcbccb1fa
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-11-03 19:40:19 -0500

    Handle failed inserts

--- test/validation_test.js
+++ test/validation_test.js
@@ -15,4 +15,14 @@ describe('Validating records', () => {
     const { message } = validateResult.errors.name;
     assert(message === 'Name must be longer than 2 characters.');
   });
+
+  it('disallows invalid records from being saved', done => {
+    const user = new User({ name: 'Al' });
+    user.save()
+      .catch(validateResult => {
+        const { message } = validateResult.errors.name;
+        assert(message === 'Name must be longer than 2 characters.');
+        done();
+      });
+  });
 });
