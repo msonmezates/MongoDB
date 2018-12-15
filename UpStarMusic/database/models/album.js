eb8529cb7719265f5e9078acaf4a31fe0dd816f9
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-12-15 12:39:40 -0600

    Create album schema

--- UpStarMusic/database/models/album.js
+++ UpStarMusic/database/models/album.js
@@ -1,1 +1,15 @@
-// Todo: create Album Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const AlbumSchema = new Schema({
+  title: String,
+  date: Date,
+  copiesSold: Number,
+  numberTracks: Number,
+  image: String,
+  revenue: Number
+});
+
+const Album = mongoose.model('album', AlbumSchema);
+
+module.exports = Album;
