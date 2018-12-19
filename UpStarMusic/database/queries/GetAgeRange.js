1b5d517e69d1a89869e032cd4abb0386cea3fcd7
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2018-12-19 08:48:59 -0600

    Implement queries that return min-max age and active years

--- UpStarMusic/database/queries/GetAgeRange.js
+++ UpStarMusic/database/queries/GetAgeRange.js
@@ -6,4 +6,11 @@ const Artist = require('../models/artist');
  * containing the min and max ages, like { min: 16, max: 45 }.
  */
 module.exports = () => {
+  const minAgeArtist = Artist.find({}).sort({ age: 1 }).limit(1).then(artists => artists[0].age);
+  const maxAgeArtist = Artist.find({}).sort({ age: -1 }).limit(1).then(artists => artists[0].age);
+
+  return Promise.all([minAgeArtist, maxAgeArtist])
+    .then(result => {
+      return { min: minAgeArtist, max: maxAgeArtist };
+    });
 };
--- UpStarMusic/database/queries/GetYearsActiveRange.js
+++ UpStarMusic/database/queries/GetYearsActiveRange.js
@@ -6,4 +6,11 @@ const Artist = require('../models/artist');
  * containing the min and max yearsActive, like { min: 0, max: 14 }.
  */
 module.exports = () => {
+  const minYearsActiveArtist = Artist.find({}).sort({ yearsActive: 1 }).limit(1).then(artists => artists[0].yearsActive);
+  const maxYearsActiveArtist = Artist.find({}).sort({ yearsActive: -1 }).limit(1).then(artists => artists[0].yearsActive);
+
+  return Promise.all([minYearsActiveArtist, maxYearsActiveArtist])
+    .then(result => {
+      return { min: minYearsActiveArtist, max: maxYearsActiveArtist };
+    });
 };
